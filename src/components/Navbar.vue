4c571ec310ddd8f9a62413b2f9e10786ce21e63f
Roey liba <rliba1990@gmail.com>
2022-09-11 00:54:08 +0300

    update blog name

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <header>
-    <h1>The Dojo Blog</h1>
+    <h1>The Vue Blog</h1>
     <nav>
       <router-link :to="{ name: 'Home' }">Home</router-link>
       <router-link :to="{ name: 'Create' }">Create Post</router-link>
